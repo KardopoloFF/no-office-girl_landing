941976d0ceeefe13e1b3a0efb2963a43f5ef0328
Nikita Kardopolov <n.kardopolov@cognitivepilot.com>
2026-03-04 21:51:11 +0300

    feat(header, index.html): шрифт Inter, Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import { Header } from "../components/Header/Header";
 import styles from "./Home.module.scss";
 
 export const HomePage = () => {
-  return <div className={styles.Container}></div>;
+  return (
+    <main className={styles.Container}>
+      <Header />
+    </main>
+  );
 };
