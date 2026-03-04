4b6f4806d65aef6bf2232833b9263c35e33182be
Nikita Kardopolov <n.kardopolov@cognitivepilot.com>
2026-03-04 14:56:30 +0300

    feat(homePage): уст. scss,  настроил архитектуру, добавил базовую страницу

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import "./App.scss";
+import "./styles/_colors.scss";
+
+import { HomePage } from "./pages/Home";
 
 function App() {
-  return <div className="App">!!!</div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
